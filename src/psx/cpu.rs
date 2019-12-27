333ff020ad1203a6afc9f904e758b9a29e3b3682
Lionel Flandrin <lionel@svkt.org>
2019-12-27 19:10:38 +0000

    Implement SLL

--- src/psx/cpu.rs
+++ src/psx/cpu.rs
@@ -74,6 +74,27 @@ pub fn run_next_instruction(psx: &mut Psx) {
     handler(psx, instruction);
 }
 
+/// When the main opcode is 0 we need to dispatch through a secondary table based on bits [5:0] of
+/// the instruction
+fn op_function(psx: &mut Psx, instruction: Instruction) {
+    let handler = FUNCTION_HANDLERS[instruction.function()];
+
+    handler(psx, instruction);
+}
+
+/// Shift Left Logical
+///
+/// `SLL $r0, $r0, 0` (machine code 0x0000_0000) is the idiomatic way of encoding a NOP
+fn op_sll(psx: &mut Psx, instruction: Instruction) {
+    let i = instruction.shift();
+    let t = instruction.t();
+    let d = instruction.d();
+
+    let v = psx.cpu.reg(t) << i;
+
+    psx.cpu.set_reg(d, v);
+}
+
 /// Bitwise Or Immediate
 fn op_ori(psx: &mut Psx, instruction: Instruction) {
     let i = instruction.imm();
@@ -126,6 +147,13 @@ impl Instruction {
         (op >> 26) as usize
     }
 
+    /// Return bits [5:0] of the instruction
+    fn function(self) -> usize {
+        let Instruction(op) = self;
+
+        (op & 0x3f) as usize
+    }
+
     /// Return immediate value in bits [16:0]
     fn imm(self) -> u32 {
         let Instruction(op) = self;
@@ -143,6 +171,13 @@ impl Instruction {
         v as u32
     }
 
+    /// Shift Immediate values are stored in bits [10:6]
+    fn shift(self) -> u32 {
+        let Instruction(op) = self;
+
+        (op >> 6) & 0x1f
+    }
+
     /// Return register index in bits [25:21]
     fn s(self) -> RegisterIndex {
         let Instruction(op) = self;
@@ -155,6 +190,13 @@ impl Instruction {
         let Instruction(op) = self;
 
         RegisterIndex((op >> 16) & 0x1f)
+    }
+
+    /// Return register index in bits [15:11]
+    fn d(self) -> RegisterIndex {
+        let Instruction(op) = self;
+
+        RegisterIndex((op >> 11) & 0x1f)
     }
 }
 
@@ -181,7 +223,7 @@ fn op_unimplemented(_psx: &mut Psx, instruction: Instruction) {
 /// Handler table for the main opcodes (instruction bits [31:26])
 const OPCODE_HANDLERS: [fn(&mut Psx, Instruction); 64] = [
     // 0x00
-    op_unimplemented,
+    op_function,
     op_unimplemented,
     op_unimplemented,
     op_unimplemented,
@@ -248,6 +290,87 @@ const OPCODE_HANDLERS: [fn(&mut Psx, Instruction); 64] = [
     op_unimplemented,
     op_unimplemented,
     op_unimplemented,
+];
+
+/// Placeholder while we haven't implemented all opcodes
+fn op_unimplemented_function(_psx: &mut Psx, instruction: Instruction) {
+    panic!(
+        "Encountered unimplemented instruction {} (function: 0x{:x})",
+        instruction,
+        instruction.function()
+    );
+}
+
+/// Handler table for the function codes (instruction bits [31:26] when opcode is 0)
+const FUNCTION_HANDLERS: [fn(&mut Psx, Instruction); 64] = [
+    // 0x00
+    op_sll,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    // 0x10
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    // 0x20
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    // 0x30
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
+    op_unimplemented_function,
 ];
 
 /// Conventional names given to the MIPS registers
