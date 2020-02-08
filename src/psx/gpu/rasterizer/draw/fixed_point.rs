8300ffc5afd2258c7aaa0c9795a086848c5cd3d4
Lionel Flandrin <lionel@svkt.org>
2020-02-08 20:20:53 +0000

    Implement triangle rasterization geometry (no shading)

--- src/psx/gpu/rasterizer/draw/fixed_point.rs
+++ src/psx/gpu/rasterizer/draw/fixed_point.rs
@@ -5,27 +5,64 @@
 #![allow(clippy::suspicious_arithmetic_impl)]
 
 use std::fmt;
-use std::ops::Div;
+use std::ops::{Add, AddAssign, Mul, Sub, SubAssign};
 
-/// The number of bits used for sub-pixel precision in a FixedPoint coordinate
-const FIXED_POINT_SHIFT: u32 = 8;
+/// The number of bits used for the fractional part of a FixedPoint value.
+///
+/// I'm not entirely sure what this value is on the real console (or even if it's really how the
+/// drawing algorithm is really implemented).
+const FIXED_POINT_SHIFT: u32 = 32;
 
 /// Fixed point representation of a number.
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
-pub struct FixedPoint(i32);
+pub struct FixedPoint(i64);
 
 impl FixedPoint {
     pub fn new(v: i32) -> FixedPoint {
-        FixedPoint(v << FIXED_POINT_SHIFT)
+        FixedPoint(i64::from(v) << FIXED_POINT_SHIFT)
     }
 
-    #[cfg(test)]
+    /// Create a new FixedPoint value that's equal to the largest possible value that's less than
+    /// `v + 1`, in other words: `v + 1 - epsilon()`
+    pub fn new_saturated(v: i32) -> FixedPoint {
+        let f = FixedPoint::new(v + 1);
+
+        f - FixedPoint::epsilon()
+    }
+
+    /// Create a new dx/dy slope ratio. The result is rounded to the available precision away from
+    /// 0. `dy` *must* be greater than 0.
+    pub fn new_dxdy(dx: i32, dy: i32) -> FixedPoint {
+        debug_assert!(dy > 0);
+
+        let mut fpdx = FixedPoint::new(dx);
+        let dy = i64::from(dy);
+
+        // We want the division to round away from 0. We know that dy is positive, so the sign of
+        // the result will be that of dx. So if dx is negative we need to do `(dx - dy + 1) / dy`
+        // and if it's positive `(dx + dy - 1) / dy`.
+        let bias = dy - 1;
+
+        if dx > 0 {
+            fpdx.0 += bias;
+        } else {
+            fpdx.0 -= bias;
+        }
+
+        FixedPoint(fpdx.0 / dy)
+    }
+
+    /// Returns the smallest possible FixedPoint value > 0
+    pub fn epsilon() -> FixedPoint {
+        FixedPoint(1)
+    }
+
     pub fn truncate(self) -> i32 {
-        self.0 >> FIXED_POINT_SHIFT
+        (self.0 >> FIXED_POINT_SHIFT) as i32
     }
 
     pub fn to_float(self) -> f32 {
-        (self.0 as f32) / ((1 << FIXED_POINT_SHIFT) as f32)
+        (self.0 as f32) / ((1i64 << FIXED_POINT_SHIFT) as f32)
     }
 }
 
@@ -35,24 +72,73 @@ impl fmt::Display for FixedPoint {
     }
 }
 
-impl Div for FixedPoint {
+impl Add for FixedPoint {
     type Output = Self;
 
-    fn div(self, rhs: Self) -> Self::Output {
-        // PSX coordinates use 16 bits, with FIXED_POINT_SHIFT at 8 we should be able to shift
-        // twice without overflow, so no need to cast to i64
-        let n = self.0 << FIXED_POINT_SHIFT;
-        let d = rhs.0;
+    fn add(self, other: Self) -> Self::Output {
+        FixedPoint(self.0 + other.0)
+    }
+}
 
-        FixedPoint(n / d)
+impl AddAssign for FixedPoint {
+    fn add_assign(&mut self, other: Self) {
+        *self = *self + other;
+    }
+}
+
+impl Sub for FixedPoint {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        FixedPoint(self.0 - other.0)
+    }
+}
+
+impl SubAssign for FixedPoint {
+    fn sub_assign(&mut self, other: Self) {
+        *self = *self - other;
+    }
+}
+
+impl Mul<i32> for FixedPoint {
+    type Output = Self;
+
+    fn mul(self, rhs: i32) -> Self::Output {
+        FixedPoint(self.0 * i64::from(rhs))
     }
 }
 
 #[test]
-fn test_divide() {
+fn test_add() {
+    let ten = FixedPoint::new(10);
+    let two = FixedPoint::new(2);
+    let mtwo = FixedPoint::new(-2);
+
+    assert_eq!(ten + two, FixedPoint::new(12));
+    assert_eq!(two + ten, FixedPoint::new(12));
+    assert_eq!(ten + mtwo, FixedPoint::new(8));
+    assert_eq!(mtwo + ten, FixedPoint::new(8));
+}
+
+#[test]
+fn test_sub() {
+    let ten = FixedPoint::new(10);
+    let two = FixedPoint::new(2);
+    let mtwo = FixedPoint::new(-2);
+
+    assert_eq!(ten - two, FixedPoint::new(8));
+    assert_eq!(two - ten, FixedPoint::new(-8));
+    assert_eq!(ten - mtwo, FixedPoint::new(12));
+    assert_eq!(mtwo - ten, FixedPoint::new(-12));
+}
+
+#[test]
+fn test_mul_i32() {
     let ten = FixedPoint::new(10);
     let two = FixedPoint::new(2);
 
-    assert_eq!(ten / two, FixedPoint::new(5));
-    assert_eq!((two / ten).truncate(), 0);
+    assert_eq!(ten * 8, FixedPoint::new(80));
+    assert_eq!(two * 13, FixedPoint::new(26));
+    assert_eq!(two * -13, FixedPoint::new(-26));
+    assert_eq!(two * 0, FixedPoint::new(0));
 }
--- src/psx/gpu/rasterizer/draw/mod.rs
+++ src/psx/gpu/rasterizer/draw/mod.rs
@@ -11,6 +11,7 @@ use crate::psx::gpu::commands::{NoShading, Position};
 use crate::psx::gpu::commands::{NoTexture, Opaque, ShadingMode, TextureMode, TransparencyMode};
 use crate::psx::gpu::{DrawMode, MaskSettings};
 use fixed_point::FixedPoint;
+use std::cmp::{max, min};
 use std::fmt;
 
 #[derive(Debug)]
@@ -191,7 +192,6 @@ impl Rasterizer {
             unimplemented!();
         } else {
             *vram_pixel = VRamPixel::from_bgr888(color);
-            println!("draw {}x{} {}", x, y, *vram_pixel);
         };
     }
 
@@ -246,11 +246,14 @@ impl Rasterizer {
         Texture: TextureMode,
         Shading: ShadingMode,
     {
-        // We're going to draw the triangle one line at a time, starting at the top and ending at
-        // the bottom. First, let's order the vertices by Y coordinate
+        // Order the vertices by y
         vertices.sort_by(|a, b| a.position.y.cmp(&b.position.y));
 
-        // Now we need to draw split the triangle in two sub-triangles. Consider the following
+        let a = &vertices[0];
+        let b = &vertices[1];
+        let c = &vertices[2];
+
+        // We need to draw split the triangle in two sub-triangles. Consider the following
         // triangle:
         //
         //    A
@@ -258,36 +261,327 @@ impl Rasterizer {
         //    |\
         //    | \
         //    |  \
-        //    |   + B <-- Need to cut horizontally here.
+        //  H +   + B <-- Need to cut horizontally here.
         //    |  /
         //    | /
         //    |/
         //    +
         //    C
         //
+        // Note that since we order A, B and C by Y coordinate it's possible for B to be on either
+        // side of the triangle (see `ac_is_left` below)
+        //
         // In order to draw it simply we need to break it into two triangles by drawing an
         // horizontal line at B. Then we can simply iterate on each line from A to B, with the line
         // width increasing by a constant amount at every step. Then we can do the same thing from
         // B to C
         //
-        // Of course in some situations we'll end up with "flat" triangles, where A.x == B.x or C.x
-        // == B.x, in which case one of these sub-triangles will effectively have 0 height.
-        let a = &vertices[0];
-        let b = &vertices[1];
-        let c = &vertices[2];
+        // Of course in some situations we'll end up with "flat" triangles, where one edge is
+        // perfectly horizontal and A.x == B.x or C.x == B.x, in which case one of these
+        // sub-triangles will effectively have 0 height.
 
-        let y_ab = b.position.y - a.position.y;
-        let y_ac = c.position.y - a.position.y;
+        let y_min = a.position.y;
+        let y_max = c.position.y;
 
-        let dx_ab = FixedPoint::new(b.position.x - a.position.x) / FixedPoint::new(y_ab);
-        let dx_ac = FixedPoint::new(c.position.x - a.position.x) / FixedPoint::new(y_ac);
+        if y_max - y_min >= 512 {
+            // Triangle is too tall, give up
+            return;
+        }
 
-        panic!("Draw triangle {:?} ({}, {})", vertices, dx_ab, dx_ac);
+        if y_max < self.clip_y_min || y_min > self.clip_y_max {
+            // The triangle is fully above or below the clip area, we don't have anything to draw
+            return;
+        }
+
+        // Find the left side of the bounding box and the index of the core vertex. The core vertex
+        // is the one we'll start drawing from.
+        let (x_min, core_index) = vertices
+            .iter()
+            .min_by(|v0, v1| {
+                // Here's the trick: the "core" vertex is the leftmost one. If two vertices are
+                // lined up vertically on the left they'll both be equally leftmost, in this case
+                // we take the one that comes *last* in the command.
+                v0.position
+                    .x
+                    .cmp(&v1.position.x)
+                    .then_with(|| v1.index.cmp(&v0.index))
+            })
+            .map(|core_vertex| (core_vertex.position.x, core_vertex.index))
+            .unwrap();
+
+        let x_max = vertices.iter().map(|v| v.position.x).max().unwrap();
+
+        if x_max - x_min >= 1024 {
+            // Triangle is too large, give up
+            return;
+        }
+
+        if x_max < self.clip_x_min || x_min > self.clip_x_max {
+            // The triangle is fully to the left or right of the draw area, we don't have anything
+            // to draw
+            return;
+        }
+
+        let xproduct = cross_product(a.position, b.position, c.position);
+
+        if xproduct == 0 {
+            // All three vertices are aligned, the triangle is perfectly flat and we have nothing
+            // to draw
+            return;
+        }
+
+        // True if AC is the left edge and AB + BC are the right edges, false if it's the other way
+        // around
+        let ac_is_left = xproduct > 0;
+
+        let a_x = a.position.x;
+        let b_x = b.position.x;
+        let c_x = c.position.x;
+
+        let a_y = a.position.y;
+        let b_y = b.position.y;
+        let c_y = c.position.y;
+
+        // Slope of AC. We've already checked that the triangle had non-0 screen height, so we know
+        // that this can't be a division by 0
+        let ac_dxdy = FixedPoint::new_dxdy(c_x - a_x, c_y - a_y);
+
+        // Slope of AB
+        let ab_dxdy = if a_y != b_y {
+            FixedPoint::new_dxdy(b_x - a_x, b_y - a_y)
+        } else {
+            // AB is horizontal, we won't have to use this variable
+            FixedPoint::new(0)
+        };
+
+        // Slope of BC
+        let bc_dxdy = if b_y != c_y {
+            FixedPoint::new_dxdy(c_x - b_x, c_y - b_y)
+        } else {
+            // BC is horizontal, we won't have to use this variable
+            FixedPoint::new(0)
+        };
+
+        let a_fpx = FixedPoint::new_saturated(a_x);
+        let b_fpx = FixedPoint::new_saturated(b_x);
+        let c_fpx = FixedPoint::new_saturated(c_x);
+        // Coordinate of the point on AC that has the same y as B
+        let h_fpx = a_fpx + ac_dxdy * (b_y - a_y);
+
+        // The draw order depends on the core_vertex.
+        if core_index == a.index {
+            // We draw AB then BC
+
+            if a_y != b_y {
+                // Draw AB
+                let (left_dxdy, right_dxdy) = if ac_is_left {
+                    (ac_dxdy, ab_dxdy)
+                } else {
+                    (ab_dxdy, ac_dxdy)
+                };
+
+                let rc = RasterCoords {
+                    start_y: a_y,
+                    end_y: b_y,
+                    left_x: a_fpx,
+                    right_x: a_fpx,
+                    left_dxdy,
+                    right_dxdy,
+                };
+
+                self.rasterize::<Transparency, Texture, Shading>(rc, RasterDir::Down);
+            }
+
+            if b_y != c_y {
+                // Draw BC
+                let (left_x, left_dxdy, right_x, right_dxdy) = if ac_is_left {
+                    (h_fpx, ac_dxdy, b_fpx, bc_dxdy)
+                } else {
+                    (b_fpx, bc_dxdy, h_fpx, ac_dxdy)
+                };
+
+                let rc = RasterCoords {
+                    start_y: b_y,
+                    end_y: c_y,
+                    left_x,
+                    right_x,
+                    left_dxdy,
+                    right_dxdy,
+                };
+
+                self.rasterize::<Transparency, Texture, Shading>(rc, RasterDir::Down);
+            }
+        } else {
+            // Core vertex is B or C
+
+            if b_y != c_y {
+                if core_index == b.index {
+                    // Draw BC
+                    let (left_x, left_dxdy, right_x, right_dxdy) = if ac_is_left {
+                        (h_fpx, ac_dxdy, b_fpx, bc_dxdy)
+                    } else {
+                        (b_fpx, bc_dxdy, h_fpx, ac_dxdy)
+                    };
+
+                    let rc = RasterCoords {
+                        start_y: b_y,
+                        end_y: c_y,
+                        left_x,
+                        right_x,
+                        left_dxdy,
+                        right_dxdy,
+                    };
+
+                    self.rasterize::<Transparency, Texture, Shading>(rc, RasterDir::Down);
+                } else {
+                    // Core vertex is C. Draw CB.
+                    let (left_dxdy, right_dxdy) = if ac_is_left {
+                        (ac_dxdy, bc_dxdy)
+                    } else {
+                        (bc_dxdy, ac_dxdy)
+                    };
+
+                    let rc = RasterCoords {
+                        start_y: c_y,
+                        end_y: b_y,
+                        left_x: c_fpx,
+                        right_x: c_fpx,
+                        left_dxdy,
+                        right_dxdy,
+                    };
+
+                    self.rasterize::<Transparency, Texture, Shading>(rc, RasterDir::Up);
+                }
+            }
+
+            // If the core vertex is B or C we always end up by drawing BA
+            if a_y != b_y {
+                let (left_x, left_dxdy, right_x, right_dxdy) = if ac_is_left {
+                    (h_fpx, ac_dxdy, b_fpx, ab_dxdy)
+                } else {
+                    (b_fpx, ab_dxdy, h_fpx, ac_dxdy)
+                };
+
+                let rc = RasterCoords {
+                    start_y: b_y,
+                    end_y: a_y,
+                    left_x,
+                    right_x,
+                    left_dxdy,
+                    right_dxdy,
+                };
+
+                self.rasterize::<Transparency, Texture, Shading>(rc, RasterDir::Up);
+            }
+        }
+    }
+
+    fn rasterize<Transparency, Texture, Shading>(&mut self, rc: RasterCoords, dir: RasterDir)
+    where
+        Transparency: TransparencyMode,
+        Texture: TextureMode,
+        Shading: ShadingMode,
+    {
+        let mut y = rc.start_y;
+        let mut left_x = rc.left_x;
+        let mut right_x = rc.right_x;
+
+        if dir == RasterDir::Up {
+            while y != rc.end_y {
+                // We move first, then we draw.
+                y -= 1;
+                left_x -= rc.left_dxdy;
+                right_x -= rc.right_dxdy;
+
+                if y < self.clip_y_min {
+                    // We left the drawing area
+                    break;
+                }
+
+                if y <= self.clip_y_max {
+                    self.rasterize_line::<Transparency, Texture, Shading>(
+                        y,
+                        left_x.truncate(),
+                        right_x.truncate(),
+                    );
+                }
+            }
+        } else {
+            while y != rc.end_y {
+                if y > self.clip_y_max {
+                    // We left the drawing area
+                    break;
+                }
+
+                if y >= self.clip_y_min {
+                    self.rasterize_line::<Transparency, Texture, Shading>(
+                        y,
+                        left_x.truncate(),
+                        right_x.truncate(),
+                    );
+                }
+
+                y += 1;
+                left_x += rc.left_dxdy;
+                right_x += rc.right_dxdy;
+            }
+        }
+    }
+
+    fn rasterize_line<Transparency, Texture, Shading>(&mut self, y: i32, left_x: i32, right_x: i32)
+    where
+        Transparency: TransparencyMode,
+        Texture: TextureMode,
+        Shading: ShadingMode,
+    {
+        let left_x = max(left_x, self.clip_x_min);
+        let right_x = min(right_x, self.clip_x_max);
+
+        if left_x >= right_x {
+            // Line is either 0-length or clipped
+            return;
+        }
+
+        // XXX fixme
+        let color = Bgr888::from_command(0x00_00ff);
+        for x in left_x..right_x {
+            self.draw_solid_pixel::<Transparency>(x, y, color);
+        }
     }
 }
 
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+enum RasterDir {
+    /// We drawing lines from top to bottom
+    Down,
+    /// We drawing lines from bottom to top
+    Up,
+}
+
+/// Structure containing the definition of the various coordinates necessary to rasterize a
+/// triangle
+struct RasterCoords {
+    /// Y coordinate of the first line to be drawn
+    start_y: i32,
+    /// Y coordinate of the first line *not* to be drawn
+    end_y: i32,
+    /// X coordinate of the left side of the first line to be drawn
+    left_x: FixedPoint,
+    /// X coordinate of the right side of the first line to be drawn
+    right_x: FixedPoint,
+    /// Value added or subtracted to left_x every time we move down or up one line (respectively)
+    left_dxdy: FixedPoint,
+    /// Value added or subtracted to right_x every time we move down or up one line (respectively)
+    right_dxdy: FixedPoint,
+}
+
+/// Compute the cross-product of (AB) x (AC)
+fn cross_product(a: Position, b: Position, c: Position) -> i32 {
+    (b.x - a.x) * (c.y - a.y) - (c.x - a.x) * (b.y - a.y)
+}
+
 /// A single BGR1555 VRAM pixel. In order to make the emulation code simpler and to support
-/// increased color depth we always use xBGR 1888 internally
+/// increased color depth we always use xRGB 1888 internally
 #[derive(Copy, Clone, PartialEq, Eq)]
 struct VRamPixel(u32);
 
@@ -297,7 +591,7 @@ impl VRamPixel {
     }
 
     fn from_bgr888(bgr: Bgr888) -> VRamPixel {
-        VRamPixel(bgr.0)
+        VRamPixel((bgr.red() << 16) | (bgr.green() << 8) | bgr.blue())
     }
 
     fn from_mbgr1555(mbgr: u16) -> VRamPixel {
@@ -312,7 +606,7 @@ impl VRamPixel {
         let g = (g << 3) | (g >> 2);
         let b = (b << 3) | (b >> 2);
 
-        VRamPixel(r | (g << 8) | (b << 16) | (m << 24))
+        VRamPixel(b | (g << 8) | (r << 16) | (m << 24))
     }
 
     fn to_rgb888(self) -> u32 {
@@ -354,8 +648,24 @@ impl fmt::Debug for VRamPixel {
 struct Bgr888(u32);
 
 impl Bgr888 {
+    fn black() -> Bgr888 {
+        Bgr888(0)
+    }
+
     fn from_command(c: u32) -> Bgr888 {
         Bgr888(c & 0xff_ffff)
+    }
+
+    fn red(self) -> u32 {
+        self.0 & 0xff
+    }
+
+    fn green(self) -> u32 {
+        (self.0 >> 8) & 0xff
+    }
+
+    fn blue(self) -> u32 {
+        (self.0 >> 16) & 0xff
     }
 }
 
@@ -365,19 +675,19 @@ struct Vertex {
     position: Position,
     color: Bgr888,
     texture: u32,
+    /// The order in which the vertices are received is sometimes important, so we keep track of
+    /// the index in the original command here.
+    index: u8,
 }
 
 impl Vertex {
-    fn new() -> Vertex {
+    fn new(index: u8) -> Vertex {
         Vertex {
             position: Position::new(0, 0),
             color: Bgr888::from_command(0),
             texture: 0,
+            index,
         }
-    }
-
-    fn set_color(&mut self, c: u32) {
-        self.color = Bgr888::from_command(c);
     }
 
     fn set_position(&mut self, p: u32) {
@@ -405,16 +715,24 @@ where
     Texture: TextureMode,
     Shading: ShadingMode,
 {
-    let mut vertices = [Vertex::new(), Vertex::new(), Vertex::new(), Vertex::new()];
+    let mut vertices = [
+        Vertex::new(0),
+        Vertex::new(1),
+        Vertex::new(2),
+        Vertex::new(3),
+    ];
 
     let mut index = 0;
+    let mut cur_color = Bgr888::black();
 
     // Load the vertex data from the command
     for (v, vertex) in vertices.iter_mut().enumerate() {
         if v == 0 || Shading::is_shaded() {
-            vertex.set_color(params[index]);
+            cur_color = Bgr888::from_command(params[index]);
             index += 1;
         }
+
+        vertex.color = cur_color;
 
         vertex.set_position(params[index]);
         index += 1;
@@ -517,16 +835,18 @@ where
     Texture: TextureMode,
     Shading: ShadingMode,
 {
-    let mut vertices = [Vertex::new(), Vertex::new(), Vertex::new()];
+    let mut vertices = [Vertex::new(0), Vertex::new(1), Vertex::new(2)];
 
     let mut index = 0;
+    let mut cur_color = Bgr888::black();
 
     // Load the vertex data from the command
     for (v, vertex) in vertices.iter_mut().enumerate() {
         if v == 0 || Shading::is_shaded() {
-            vertex.set_color(params[index]);
+            cur_color = Bgr888::from_command(params[index]);
             index += 1;
         }
+        vertex.color = cur_color;
 
         vertex.set_position(params[index]);
         index += 1;
@@ -814,8 +1134,8 @@ pub static GP0_COMMANDS: [CommandHandler; 0x100] = [
     },
     // 0x20
     CommandHandler {
-        handler: cmd_unimplemented,
-        len: 1,
+        handler: cmd_handle_poly_tri::<Opaque, NoTexture, NoShading>,
+        len: 4,
     },
     CommandHandler {
         handler: cmd_unimplemented,
